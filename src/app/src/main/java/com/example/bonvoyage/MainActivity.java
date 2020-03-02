e2e059ae43637c91a003f1dd31a94d5f15ba3e6a
Nicole <nhagerma@ualberta.ca>
2020-03-02 11:45:21 -0700

    Revert "added layout for rider status"

--- src/app/src/main/java/com/example/bonvoyage/MainActivity.java
+++ src/app/src/main/java/com/example/bonvoyage/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.bonvoyage;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
