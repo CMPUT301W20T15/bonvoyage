ddbcdaecb8298c9b06f1548542d3bbb988e47586
Nick Anderson <47461646+nick98anderson@users.noreply.github.com>
2020-03-02 18:59:00 -0700

    added fields to RiderStatusDialog
    added fields and changed some id's

--- src/app/src/main/java/com/example/bonvoyage/MainActivity.java
+++ src/app/src/main/java/com/example/bonvoyage/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bonvoyage.fragments.RiderStatusDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RiderStatusDialog dialog = new RiderStatusDialog();
+        dialog.show(getSupportFragmentManager(), "dialog");
 
     }
 }
--- src/app/src/main/java/com/example/bonvoyage/fragments/RiderStatusDialog.java
+++ src/app/src/main/java/com/example/bonvoyage/fragments/RiderStatusDialog.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,14 +17,64 @@ import com.example.bonvoyage.R;
 public class RiderStatusDialog extends DialogFragment {
 
     private TextView title;
+    private TextView driverName;
+    private TextView driverVehicle;
+    private TextView currentLocation;
+    private TextView destination;
+    private TextView amount;
+    private Button callBtn;
+    private Button textBtn;
+    private ImageView priceIcon;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.rider_request_status, container, false);
         title = view.findViewById(R.id.rider_status_title);
+        driverName = view.findViewById(R.id.ds_driver_name);
+        driverVehicle = view.findViewById(R.id.rs_car_type);
+        currentLocation = view.findViewById((R.id.rs_location));
+        destination = view.findViewById(R.id.rs_destination);
+        amount = view.findViewById(R.id.rs_price);
+        callBtn = view.findViewById(R.id.rs_call_btn);
+        textBtn = view.findViewById(R.id.rs_text_btn);
+        priceIcon = view.findViewById(R.id.rs_amount_icon);
+
+        if(isAccepted() && !isInProgress()){
+            title.setText("Your driver is on their way!");
+        }
+
+        else if(isAccepted() && isInProgress()){
+            title.setText("Your ride is in progress!");
+            amount.setVisibility(View.GONE);
+            callBtn.setVisibility(View.GONE);
+            textBtn.setVisibility(View.GONE);
+            priceIcon.setVisibility(View.GONE);
+        }
+
+
         return view;
     }
+
+
+    //Not implemented yet. used for xml layout testing.
+    public boolean isAccepted(){
+        return true;
+    }
+
+    //not actual implementation. used for xml layout testing
+    public boolean isInProgress(){
+        return true;
+    }
+
+
+
+
+
+
+
+
 
 
 
