632978c8162cbcf744e85106899a930c1c14b308
anasiv <anasiv350@gmail.com>
2020-03-09 13:57:20 -0600

    Driver and Rider payment

---
+++ app/src/main/java/com/example/bonvoyage/DriverPayment.java
@@ -0,0 +1,142 @@
+/**
+ * instructions for the code from
+ * https://www.journaldev.com/18198/qr-code-barcode-scanner-android
+ */
+package com.example.bonvoyage;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class DriverPayment extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    TextView barcodeVal;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    Button doneButton;
+    String intentData = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_barcode);
+
+        barcodeVal = findViewById(R.id.barcode_value);
+        surfaceView = findViewById(R.id.surfaceView);
+        doneButton = findViewById(R.id.done_button);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (intentData.length() > 0) {
+                    startActivity(new Intent(DriverPayment.this, Activity.class));  // Set firebase save function in the second argument
+                } else {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(intentData)));
+                }
+            }
+        });
+    }
+
+    private void initialiseDetectorsAndSources() {
+
+        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) // enables auto focus for the camera
+                .build();
+
+        // gets the permissions for using the camera functions
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(DriverPayment.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(DriverPayment.this, new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+
+                    // sets the button text val after finding a barcode
+                    barcodeVal.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            barcodeVal.removeCallbacks(null);
+                            intentData = barcodes.valueAt(0).toString();
+                            barcodeVal.setText(intentData);
+                            doneButton.setText("ADD TO WALLET");
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initialiseDetectorsAndSources();
+    }
+}
--- app/src/main/java/com/example/bonvoyage/MainActivity.java
+++ app/src/main/java/com/example/bonvoyage/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.bonvoyage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
