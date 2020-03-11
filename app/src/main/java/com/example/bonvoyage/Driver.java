cd45593f4f9fb3c15b7e3c99f94d2f8679aacf1f
nicolelisa <nhagerma@ualberta.ca>
2020-03-11 17:48:22 -0600

    fix build issue

--- app/src/main/java/com/example/bonvoyage/Driver.java
+++ app/src/main/java/com/example/bonvoyage/Driver.java
@@ -1,8 +1,7 @@
 package com.example.bonvoyage;
 
 public class Driver extends User {
-    // Nearby RideRequests
-        //
+
 
     public Driver(String firstname, String lastname, String email, String phonenumber, String password) {
         super(firstname, lastname, email, phonenumber, password);
