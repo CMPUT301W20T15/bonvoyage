d5c016c1d985ddce12bf996e760f4554030ca0c1
anasiv <anasiv350@gmail.com>
2020-03-25 14:02:04 -0600

    adding wallet variable to user, rider, and driver

--- app/src/main/java/com/example/bonvoyage/Driver.java
+++ app/src/main/java/com/example/bonvoyage/Driver.java
@@ -3,7 +3,7 @@ package com.example.bonvoyage;
 public class Driver extends User {
 
 
-    public Driver(String firstname, String lastname, String email, String phonenumber, String password) {
-        super(firstname, lastname, email, phonenumber, password);
+    public Driver(String firstname, String lastname, String email, String phonenumber, String password, int wallet) {
+        super(firstname, lastname, email, phonenumber, password, wallet);
     }
 }
---
+++ app/src/main/java/com/example/bonvoyage/DriverPostPayment.java
@@ -0,0 +1,11 @@
+package com.example.bonvoyage;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DriverPostPayment extends AppCompatActivity {
+    Driver driver;
+
+    Intent intent = new Intent(DriverPostPayment.this, DriverMapActivity.class);
+}
