c8ea4160d58fc21ecde1e009553eda06616b15b1
orackle <debangan@ualberta.ca>
2020-03-24 23:13:42 -0600

    initial commit

--- app/src/androidTest/java/com/example/bonvoyage/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bonvoyage/ExampleInstrumentedTest.java
@@ -10,18 +10,60 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+///**
+// * Instrumented test, which will execute on an Android device.
+// *
+// * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+// */
+//@RunWith(AndroidJUnit4.class)
+//public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//
+//        assertEquals("com.example.bonvoyage", appContext.getPackageName());
+//    }
+//}
+import android.test.ActivityInstrumentationTestCase2;
+import com.robotium.solo.Solo;
 
-        assertEquals("com.example.bonvoyage", appContext.getPackageName());
+@SuppressWarnings("rawtypes")
+public class ExampleTest extends ActivityInstrumentationTestCase2 {
+    private Solo solo;
+    private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.example.ExampleActivty";
+
+    private static Class<?> launcherActivityClass;
+    static{
+        try {
+            launcherActivityClass = Class.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public ExampleTestTest() throws ClassNotFoundException {
+        super(launcherActivityClass);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation());
+        getActivity();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+
+    public void testRun() {
+        //Wait for activity: 'com.example.ExampleActivty'
+        solo.waitForActivity("ExampleActivty", 2000);
+        //Clear the EditText editText1
+        solo.clearEditText((android.widget.EditText) solo.getView("editText1"));
+        solo.enterText((android.widget.EditText) solo.getView("editText1"), "This is an example text");
     }
 }
---
+++ app/src/androidTest/java/com/example/bonvoyage/RiderMapActivityTest.java
@@ -0,0 +1,8 @@
+import android.test.ActivityInstrumentationTestCase2;
+import com.robotium.solo.Solo;
+
+@SuppressWarnings("rawtypes")
+public class RiderMapActivityTest extends ActivityInstrumentationTestCase2{
+    public RiderMapActivityTest() {
+    }
+}
