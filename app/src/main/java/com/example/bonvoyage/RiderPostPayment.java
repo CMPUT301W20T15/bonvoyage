8c478f8775d126c3df87928f9646a1a6aa145eae
anasiv <anasiv350@gmail.com>
2020-03-25 19:04:18 -0600

    updating

--- app/src/main/java/com/example/bonvoyage/RiderPostPayment.java
+++ app/src/main/java/com/example/bonvoyage/RiderPostPayment.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RiderPostPayment extends AppCompatActivity {
+    
+    Intent intent = new Intent(RiderPostPayment.this, RiderRatingFragment.class);
 
 
 }
