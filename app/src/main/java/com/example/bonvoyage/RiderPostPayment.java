6a67934ea00855545a0d3db2c0f6e66278cb468f
nmrchvz <46454559+nmrchvz@users.noreply.github.com>
2020-03-27 15:55:51 -0600

    Modified transactions between driver and rider

--- app/src/main/java/com/example/bonvoyage/RiderPostPayment.java
+++ app/src/main/java/com/example/bonvoyage/RiderPostPayment.java
@@ -5,19 +5,28 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RiderPostPayment extends AppCompatActivity {
     private FirebaseHandler firebaseHandler;
+    private FirebaseFirestore db;
 
     protected void onCreateView(Bundle savedInstanceState) {
-        Rider rider = firebaseHandler.getCurrentUser();
-
-        float cost = firebaseHandler.getCostOfRideFromDatabase(RiderPricingFragment.getRequestId());
-
-        firebaseHandler.riderTransaction(rider, cost);
-
-        startActivity(new Intent(RiderPostPayment.this, RiderRatingFragment.class));
+        FirebaseUser fb_rider = firebaseHandler.getCurrentUser();
+        DocumentReference docRef = db.collection("riders").document(fb_rider.getEmail());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Rider rider = documentSnapshot.toObject(Rider.class);
+                float cost = firebaseHandler.getCostOfRideFromDatabase(RiderPricingFragment.getRequestId());
+                firebaseHandler.riderTransaction(rider, cost);
+                startActivity(new Intent(RiderPostPayment.this, RiderRatingFragment.class));
+            }
+        });
     }
 
 }
