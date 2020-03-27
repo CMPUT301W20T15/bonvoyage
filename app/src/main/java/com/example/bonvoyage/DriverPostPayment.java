38be7fd43369cec306a9c035462a193bc8e45e87
anasiv <anasiv350@gmail.com>
2020-03-27 15:17:47 -0600

    getting the payment function integrated

--- app/src/main/java/com/example/bonvoyage/DriverPostPayment.java
+++ app/src/main/java/com/example/bonvoyage/DriverPostPayment.java
@@ -1,10 +1,23 @@
 package com.example.bonvoyage;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DriverPostPayment extends AppCompatActivity {
+    private FirebaseHandler firebaseHandler;
+    float amount;
 
-    Intent intent = new Intent(DriverPostPayment.this, DriverMapActivity.class);
+    protected void onCreateView(Bundle savedInstanceState) {
+
+        Driver driver = firebaseHandler.getCurrentUser();
+
+        float cost = firebaseHandler.getCostOfRideFromDatabase(RiderPricingFragment.getRequestId());
+
+        firebaseHandler.driverTransaction(driver, cost);
+
+        startActivity(new Intent(DriverPostPayment.this, DriverMapActivity.class));
+    }
+
 }
--- app/src/main/java/com/example/bonvoyage/RiderPostPayment.java
+++ app/src/main/java/com/example/bonvoyage/RiderPostPayment.java
@@ -1,12 +1,24 @@
 package com.example.bonvoyage;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RiderPostPayment extends AppCompatActivity {
-    
-    Intent intent = new Intent(RiderPostPayment.this, RiderRatingFragment.class);
+import com.google.firebase.auth.FirebaseUser;
 
+public class RiderPostPayment extends AppCompatActivity {
+
+    private FirebaseHandler firebaseHandler;
+
+    protected void onCreateView(Bundle savedInstanceState) {
+        Rider rider = firebaseHandler.getCurrentUser();
+
+        float cost = firebaseHandler.getCostOfRideFromDatabase(RiderPricingFragment.getRequestId());
+
+        firebaseHandler.riderTransaction(rider, cost);
+
+        startActivity(new Intent(RiderPostPayment.this, RiderRatingFragment.class));
+    }
 
 }
