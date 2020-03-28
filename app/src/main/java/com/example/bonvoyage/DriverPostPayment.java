a4d9f0f6a74130e1597093d66e4c795a9b2a615b
orackle <debangan@ualberta.ca>
2020-03-27 23:13:33 -0600

    change

--- app/src/main/java/com/example/bonvoyage/DriverPostPayment.java
+++ app/src/main/java/com/example/bonvoyage/DriverPostPayment.java
@@ -1,10 +1,33 @@
 package com.example.bonvoyage;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DriverPostPayment extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-    Intent intent = new Intent(DriverPostPayment.this, DriverMapActivity.class);
+public class DriverPostPayment extends AppCompatActivity {
+    private FirebaseHandler firebaseHandler;
+    private FirebaseFirestore db;
+
+    protected void onCreateView(Bundle savedInstanceState) {
+        FirebaseUser fb_driver = firebaseHandler.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("drivers").document(fb_driver.getEmail());
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Driver driver = documentSnapshot.toObject(Driver.class);
+                float cost = firebaseHandler.getCostOfRideFromDatabase(RiderPricingFragment.getRequestId());
+                firebaseHandler.driverTransaction(driver, cost);
+                startActivity(new Intent(DriverPostPayment.this, DriverMapActivity.class));
+            }
+        });
+    }
 }
