87dde2878a5ed026daba237adad8936e0be98651
orackle <debangan@ualberta.ca>
2020-03-28 01:04:13 -0600

    Revert "new"
    This reverts commit 31bdc31f624ca33dd0d9ce422bc54caf0fa5ce3a.

--- app/src/main/java/com/example/bonvoyage/LoginSignupActivity.java
+++ app/src/main/java/com/example/bonvoyage/LoginSignupActivity.java
@@ -5,11 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class LoginSignupActivity extends AppCompatActivity {
 
@@ -17,35 +12,6 @@ public class LoginSignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_login);
-        ImageView bvlogo = findViewById(R.id.bvlogo);
-        ImageView bgApp = findViewById(R.id.bgApp2);
-        bgApp.animate().translationY(-1000).setDuration(1300).setStartDelay(2000);
-        ImageView signInEmail =findViewById(R.id.signInEmailButton);
-        ImageView SignInPhone = findViewById(R.id.signInPhoneButton);
-        ImageView SignUp = findViewById(R.id.signUpButton);
-        bvlogo.animate()
-                .translationY(420)
-                .translationX(400)
-                .alpha(1.0f)
-                .setDuration(2000)
-                .setStartDelay(3000);
-        signInEmail.animate()
-                .translationY(-600)
-                .alpha(1.0f)
-                .setDuration(1000)
-                .setStartDelay(4000);
-        SignInPhone.animate()
-                .translationY(-400)
-                .alpha(1.0f)
-                .setDuration(1000)
-                .setStartDelay(4000);
-        SignUp.animate()
-                .translationY(-200)
-                .alpha(1.0f)
-                .setDuration(1000)
-                .setStartDelay(4000);
-
-
     }
     public void signInEmailClicked(View view) {
         Intent intent = new Intent(this, SignInEmailActivity.class);
