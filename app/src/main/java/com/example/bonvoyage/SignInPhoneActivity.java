aa1261ba731266b7ad5bf0ea557bf98b44afae9c
jha8 <Joeha909@gmail.com>
2020-03-28 12:22:52 -0600

    New Phone Sign In

--- app/src/main/java/com/example/bonvoyage/SignInPhoneActivity.java
+++ app/src/main/java/com/example/bonvoyage/SignInPhoneActivity.java
@@ -1,53 +1,164 @@
 package com.example.bonvoyage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * SignInPhoneActivity implements phone number authentication sign in.
  * Source code used: https://firebase.google.com/docs/auth/android/phone-auth.
  */
 public class SignInPhoneActivity extends AppCompatActivity {
+    private final String TAG = "SignInPhoneActivity";
     //Instance variables
-    private static final String TAG = "SignInPhoneActivity";
-    private EditText phoneEditText;
-    private Button submitBtn;
+    private CountryCodePicker ccp;
+    private EditText phoneText;
+    private EditText codeText;
+    private Button continueNextBtn;
+    private String checker = "";
+    private String phoneNumber = "";
+    private RelativeLayout relativeLayout;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_phone);
-        phoneEditText = findViewById(R.id.editTextPhone);
-        submitBtn = (Button) findViewById(R.id.phoneContinue);
 
-        /**
-         * If the phone number submitted is invalid/empty, then a toast will appear to
-         * prompt the user to enter a valid phone number.
-         * Once a correct phoneNumber is implemented, it redirects to a SMSActivity where
-         * the user is prompted to enter their SMS verification code.
-         */
-        submitBtn.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.setLanguageCode("it");
+        loadingBar = new ProgressDialog(SignInPhoneActivity.this);
+
+
+
+        phoneText = findViewById(R.id.phoneText);
+        codeText = findViewById(R.id.codeText);
+        continueNextBtn = findViewById(R.id.continueNextButton);
+        relativeLayout = findViewById(R.id.phoneAuth);
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        ccp.registerCarrierNumberEditText(phoneText);
+
+        continueNextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String phone = phoneEditText.getText().toString();
-                if (phone.isEmpty() || phone.length() < 11){
-                    toastMessage("Valid Number is Required");
-                }else{
-                    String completePhone = "+" + phone;
-                    Intent intent = new Intent(SignInPhoneActivity.this,SMSActivity.class);
-                    intent.putExtra("phone",completePhone);
-                    startActivity(intent);
+                if (continueNextBtn.getText().equals("Submit") || checker.equals("Code Sent")){
+                    String verifcationCode = codeText.getText().toString();
+                    if (verifcationCode.equals("")){
+                        toastMessage("Please write verification code.");
+                    }else {
+                        loadingBar.setTitle("SMS Code Verification");
+                        loadingBar.setMessage("Please wait, while we verify your SMS Code.");
+                        loadingBar.setCanceledOnTouchOutside(false);
+                        loadingBar.show();
+
+                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verifcationCode);
+                        signInWithPhoneAuthCredential(credential);
+                    }
+                }else {
+                    phoneNumber = ccp.getFullNumberWithPlus();
+                    if (!phoneNumber.equals("")){
+                        loadingBar.setTitle("Phone Number Verification");
+                        loadingBar.setMessage("Please wait, while we verify your phone number.");
+                        loadingBar.setCanceledOnTouchOutside(false);
+                        loadingBar.show();
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber, 60, TimeUnit.SECONDS, SignInPhoneActivity.this,mCallbacks);
+                        //send SMS code
+                    }else {
+                        toastMessage("Invalid Phone Number, Please Try Again!");
+                    }
                 }
             }
         });
-    }
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                toastMessage("Invalid Phone Number");
+                loadingBar.dismiss();
+                relativeLayout.setVisibility(View.VISIBLE);
+                continueNextBtn.setText("Continue");
+                codeText.setVisibility(View.GONE);
+            }
 
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                mVerificationId = s;
+                mResendToken = forceResendingToken;
+
+                relativeLayout.setVisibility(View.GONE);
+                checker = "Code Sent";
+                continueNextBtn.setText("Submit");
+                codeText.setVisibility(View.VISIBLE);
+
+                loadingBar.dismiss();
+                toastMessage("SMS Code Sent!");
+            }
+        };
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCredential:success");
+                            //FirebaseUser user = task.getResult().getUser();
+                            loadingBar.dismiss();
+                            toastMessage("Login Successful");
+                            sendUserToMainActivity();
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                toastMessage("Verification code entered was invalid");
+                            }else {
+                                toastMessage("Error: " + task.getException().toString());
+                            }
+                        }
+                    }
+                });
+    }
+    private void sendUserToMainActivity(){
+        //Intent intent = new Intent(SignInPhoneActivity.this,DriverMapActivity.this);
+        //startActivity(intent);
+        //finish();
+        toastMessage("Pass to next activity");
+    }
     /**
      * toastMessage generates a toast message.
      * @param message
