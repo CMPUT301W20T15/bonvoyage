1a7816698c2dc7c23dabadfec9323e8d0eb97d5f
orackle <debangan@ualberta.ca>
2020-03-27 22:30:21 -0600

    Update RiderPricingFragment.java

--- app/src/main/java/com/example/bonvoyage/RiderPricingFragment.java
+++ app/src/main/java/com/example/bonvoyage/RiderPricingFragment.java
@@ -18,37 +18,32 @@ import java.util.Locale;
 
 
 public class RiderPricingFragment extends Fragment {
-	private EditText priceEdit;
-	private static final String TAG = "RiderPricingFragment";
-		// The onCreateView method is called when Fragment should create its View object hierarchy,
-		// either dynamically or via XML layout inflation.
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-			return inflater.inflate(R.layout.rider_add_price, parent, false);
-		}
-
-		// This event is triggered soon after onCreateView().
-		// Any view setup should occur here.  E.g., view lookups and attaching view listeners.
-		@Override
-		public void onViewCreated(View view, Bundle savedInstanceState) {
-			priceEdit = view.findViewById(R.id.price_edit);
-		}
 
 
-		public void updatePrice() {
-			Bundle bundle = this.getArguments();
-			FirebaseHandler firebaseHandler = new FirebaseHandler();
-			if (bundle != null) {
-				HashMap tripData = (HashMap) bundle.getSerializable("HashMap");
-				float newCost = Float.parseFloat(priceEdit.getText().toString());
-				tripData.put("cost", newCost);
-				firebaseHandler.addNewRideRequestToDatabase(tripData, "hello@gmail.com");
-			}
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.rider_add_price, parent, false);
+    }
 
-		}
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        priceEdit = view.findViewById(R.id.price_edit);
+    }
+
+    public void updatePrice() {
+        Bundle bundle = this.getArguments();
+        FirebaseHandler firebaseHandler = new FirebaseHandler();
+        if (bundle != null) {
+            HashMap tripData = (HashMap) bundle.getSerializable("HashMap");
+            float newCost = Float.parseFloat(priceEdit.getText().toString());
+            tripData.put("cost", newCost);
+            firebaseHandler.addNewRideRequestToDatabase(tripData, requestId);
 
 
-
-
+        }
 
 }
